7635b7830b5c01af3f8e7afa19c65eacd5a7c304
baduc259 <ducthien25092k5@gmail.com>
2026-08-06 17:27:35 +0700

    up  admin controller

--- language_learning_backend/src/modules/languages/controllers/admin_languages.controller.ts
+++ language_learning_backend/src/modules/languages/controllers/admin_languages.controller.ts
@@ -1,10 +1,22 @@
-import { Controller, Query, UseGuards, Get, Param } from '@nestjs/common';
+import {
+  Controller,
+  Query,
+  UseGuards,
+  Get,
+  Param,
+  Post,
+  Body,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 import { Roles } from '../../../common/decorators/roles.decorator';
 import { UserRole } from '../../../common/enums/user_role.enum';
 import { JwtAuthGuard } from '../../../common/guards/jwt_auth.guard';
 import { RolesGuard } from '../../../common/guards/roles.guard';
 import { LanguagesService } from '../languages.service';
 import { LanguageQueryDto } from '../dto/language_query.dto';
+import { CreateLanguageDto } from '../dto/create_language.dto';
+import { UpdateLanguageDto } from '../dto/update_language.dto';
 
 @Controller('admin/languages')
 @UseGuards(JwtAuthGuard, RolesGuard)
@@ -18,5 +30,17 @@ export class AdminLanguagesController {
   @Get(':id')
   findOne(@Param(':id') id: string) {
     return this.laguageService.findOneForAdmin(id);
+  }
+  @Post() create(@Body() dto: CreateLanguageDto) {
+    return this.laguageService.create(dto);
+  }
+  @Patch(':id') update(
+    @Param('id') id: string,
+    @Body() dto: UpdateLanguageDto,
+  ) {
+    return this.laguageService.update(id, dto);
+  }
+  @Delete(':id') remove(@Param('id') id: string) {
+    return this.laguageService.remove(id);
   }
 }
